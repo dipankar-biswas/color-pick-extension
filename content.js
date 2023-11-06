e36bea7f31a842bae182bd976d542af2c5cf543f
Dipankar Biswas <themedesigner3@gmail.com>
2023-11-07 02:42:13 +0600

    update

--- content.js
+++ content.js
@@ -1,7 +1,63 @@
+getData();
+let code = document.querySelector('#aac-hex-name');
+let name = document.querySelector('#aac-artyclick-name');
+let showData = document.querySelector('.dataDisplay');
+let getAllData = [];
+let lastId = null;
+
 document.addEventListener('mouseup', function(event) {
-    // alert('Ok baby');
     setTimeout(() => {
-        console.log(document.querySelector('#aac-hex-name').innerHTML);
-        console.log(document.querySelector('#aac-artyclick-name').innerHTML);
+        if(code && name){
+            const filteredData = getAllData.filter((item) => {
+                return item.code === code.innerHTML;
+            });
+            if(filteredData.length > 0){
+                console.log('Color Match!! Not Inserted');
+            }else {
+                let postData = {
+                    id: lastId+1,
+                    code: document.querySelector('#aac-hex-name').innerHTML,
+                    name: document.querySelector('#aac-artyclick-name').innerHTML,
+                };
+                insertData(postData);
+            }
+        }
     }, 100);
 });
+
+
+
+function insertData(postData){
+    fetch('http://localhost:3000/colors', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('insert success:', data);
+        getData();
+    })
+    .catch((error) => {
+        console.error('Error inserting data:', error);
+    });
+}
+
+function getData(){
+    fetch('http://localhost:3000/colors', { 
+        method: 'GET'
+    })
+    .then(function(response) { return response.json(); })
+    .then(data => {
+        getAllData = data;
+        lastId = Math.max(...data.map((item) => item.id));
+        console.log('Last ID:', lastId);
+        // json.map(data => {
+        // })
+    })
+    .catch((error) => {
+        console.error('Error Get data:', error);
+    });
+}
--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-document.querySelector('.dataDisplay').innerHTML = 'SHDGIhusd sd';
+let dataDisplay = document.querySelector('.dataDisplay');
+let msg = document.querySelector('.ex-wrapper .msg');
+msg.innerHTML = 'Loading...';
+
+fetch('http://localhost:3000/colors')
+.then((response) => response.json())
+.then((data) => {
+    if(data.length > 0){
+        msg.innerHTML = '';
+        msg.classList.remove = 'show';
+        // Extract the last 10 items from the data
+        const last10Items = data.slice(-20);
+    
+        last10Items.forEach((item) => {
+            // Create a list item element
+            const listItem = document.createElement('li');
+            listItem.classList.add( "item" );
+          
+            // Create and set the content for the list item
+            const content = document.createElement('div');
+            content.classList.add( "color" );
+            content.innerHTML = `<span style="background:${item.code};width:16px;height:16px;display:block;border-radius:50%"></span><span>${item.name}</span>`;
+          
+            // Append the content to the list item
+            listItem.appendChild(content);
+          
+            // Append the list item to the ul element
+            dataDisplay.appendChild(listItem);
+        });
+    }else{
+        msg.innerHTML = 'Color Not Found!!';
+        msg.classList.add = 'show';
+    }
+})
+.catch((error) => {
+    console.error('Error fetching data:', error);
+});
+
